22bb54578863fb9ff2897ed80aa5dd5f9e2d6434
fred <fred.gauthier.dev@gmail.com>
2018-02-01 16:17:12 +0100

    Pause 16h15, temps passé 4h

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Entity\Movie;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends Controller
 {
@@ -15,6 +18,12 @@ class AdminController extends Controller
      */
     public function index()
     {
+        $books = $this->getDoctrine()->getRepository(Book::class)->findAll();
+        $movies = $this->getDoctrine()->getRepository(Movie::class)->findAll();
 
+        return $this->render('admin/index.html.twig', [
+            'books'     => $books,
+            'movies'    => $movies
+        ]);
     }
 }
