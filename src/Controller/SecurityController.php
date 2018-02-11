22dc33ac33026fcea00d7b482c6bee2d7de0235a
fred <fred.gauthier.dev@gmail.com>
2018-02-11 15:18:24 +0100

    Add a form login
    Add a form login (form + controller + view) and add fontawesone CDN

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\LoginType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     * @param Request $request
+     * @param AuthenticationUtils $authUtils
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function login(Request $request, AuthenticationUtils $authUtils){
+        // get the login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authUtils->getLastUsername();
+
+        $form = $this->createForm(LoginType::class, [
+            '_username' => $lastUsername
+        ]);
+
+        return $this->render('security/login.html.twig', array(
+            'form'          => $form->createView(),
+            'error'         => $error,
+        ));
+    }
+}
