91ba9aab81475288d11e4edd8d37aa01b580e573
fred <fred.gauthier.dev@gmail.com>
2018-02-13 15:03:32 +0100

    Add Book & Movie Subscriber

---
+++ src/EventListener/BookSubscriber.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: fred
+ * Date: 13/02/18
+ * Time: 14:50
+ */
+
+namespace App\EventListener;
+
+
+class BookSubscriber
+{
+
+}
---
+++ src/EventListener/MovieSubscriber.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: fred
+ * Date: 13/02/18
+ * Time: 15:00
+ */
+
+namespace App\EventListener;
+
+
+class MovieSubscriber
+{
+
+}
